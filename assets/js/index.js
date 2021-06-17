428e1088aa20ad0687c38171a8ef027b8fb46449
Asaroz <77623721+Asaroz@users.noreply.github.com>
2021-06-17 14:05:50 +0200

    first draft

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,4 +26,65 @@ const books = [
     alreadyRead: false,
     img: "https://eloquentjavascript.net/img/cover.jpg"
   }
-];
+]
+
+books.sort(function(a,b){
+if(a.author.split(" ")[1]<b.author.split(" ")[1]){return -1}
+if(a.author.split(" ")[1]>b.author.split(" ")[1]){return 1}
+return 0
+}
+)
+
+
+
+const container=document.querySelector(".book-list")
+container.style.display="flex"
+container.style.flexDirection="row"
+
+
+books.forEach(item=>{
+  const cardContainer = document.createElement("ul")
+  const card = document.createElement("div")
+  const img = document.createElement("img")
+  const cardBody = document.createElement("div")
+  const cardTitle = document.createElement("h6")
+  const author = document.createElement("p")
+  const footer = document.createElement("footer")
+
+  card.classList.add("card")
+  card.style.width="15rem"
+
+  img.src=item.img
+  img.classList.add("card-img-top")
+
+  cardBody.classList.add("card-body")
+
+  cardTitle.classList.add("card-title")
+  cardTitle.innerHTML = item.title
+
+  author.classList.add("card-text")
+  author.innerText= item.author
+
+  footer.classList.add("card-footer")
+  footer.innerHTML= '<span class="badge badge-secondary">'+item.alreadyRead ? "Read":"not Read"+'</span>'
+  footer.style.margin="-20px"
+
+  container.after(cardContainer)
+  cardContainer.appendChild(card)
+  card.appendChild(img)
+  card.appendChild(cardBody)
+  cardBody.appendChild(cardTitle)
+  cardBody.appendChild(author)
+  cardBody.appendChild(footer)
+})
+
+{/* <ul>
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="http://t2.gstatic.com/images?q=tbn:ANd9GcRqNE0qeS4ldVIC9DbGkx9MOwJ4WWKi6HVvtrtZ8XTKVodonSBy" alt="Card image cap">
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>    <span class="badge badge-secondary">New</span>
+</ul> */}
